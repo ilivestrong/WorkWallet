a562da14783e9f7465943969a1bb667abeeea4a0
deepak <imdeepakpathak@gmail.com>
2023-12-08 16:56:27 +0530

    test: add few tests

---
+++ app.test.js
@@ -0,0 +1,71 @@
+const request = require('supertest');
+const app = require('./src/app.js');
+
+describe('GET /contracts/:id', () => {
+    it('should successfully return contract by id', async () => {
+        const contractId = 1;
+        const profile_id = 5;
+        const response = await request(app)
+            .get(`/contracts/${contractId}`)
+            .set('profile_id', profile_id)
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('result')
+        expect(response.body.result.contract).toHaveProperty('id', 1)
+    });
+
+    it('should return contract not found, with invalid contract id', async () => {
+        const invalid_contractId = 101;
+        const profile_id = 5;
+        const response = await request(app)
+            .get(`/contracts/${invalid_contractId}`)
+            .set('profile_id', profile_id)
+
+        expect(response.statusCode).toBe(404);
+        expect(response.body).toHaveProperty('result')
+        expect(response.body.result).toHaveProperty('error', 'no contract found')
+    });
+
+    it('should return Unauthorized if no profile_id provided', async () => {
+        const invalid_contractId = 101;
+        const response = await request(app)
+            .get(`/contracts/${invalid_contractId}`)
+
+        expect(response.statusCode).toBe(401);
+    });
+});
+
+describe('GET /jobs/unpaid', () => {
+    it('should successfully return unpaid jobs when profile is Client', async () => {
+        const profile_id = 1; //profile is client
+        const response = await request(app)
+            .get(`/jobs/unpaid`)
+            .set('profile_id', profile_id)
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.result).toHaveProperty('unpaid_jobs')
+        expect(response.body.result.unpaid_jobs).toHaveLength(2)
+    });
+
+    it('should successfully return unpaid jobs when profile is Contractor', async () => {
+        const profile_id = 6; //profile is contractor
+        const response = await request(app)
+            .get(`/jobs/unpaid`)
+            .set('profile_id', profile_id)
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.result).toHaveProperty('unpaid_jobs')
+        expect(response.body.result.unpaid_jobs).toHaveLength(5)
+    });
+
+    it('should return Unauthorized if invalid profile_id provided', async () => {
+        const invalid_profile_id = 101; // God knows who is this
+        const response = await request(app)
+            .get(`/jobs/unpaid`)
+            .set('profile_id', invalid_profile_id)
+
+        expect(response.statusCode).toBe(401);
+    });
+
+   
+});
